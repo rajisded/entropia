42bd467c4a1aa1705b6c4d1ae1ba05d4f45437dc
rajisded <rajisded@icloud.com>
2026-02-13 22:56:33 +0530

    fix: hardcoded supabase credentials to resolve build error

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://fganghgfheknzpkdedjn.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnYW5naGdmaGVrbnpwa2RlZGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2MDI3NzQsImV4cCI6MjA4NTE3ODc3NH0.3WPgOHgvLGA7nWyYGfd7HYKmtoCqNaa19tPApNv6xRs'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
